f996b025c794739c822a35e5d07ea9d8260f1ce5
Crowdbotics <team@crowdbotics.com>
2024-03-03 17:19:54 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "round-thunder-46773",
+  url: "https://round-thunder-46773.botics.co",
+  api: "https://round-thunder-46773.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
